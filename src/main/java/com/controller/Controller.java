ca2d0ac7d5ae525f821836dfc1d64981447ddb37
Danil56 <danil.yeshenko@gmail.com>
2018-09-30 01:24:48 +0300

    Update .gitignore, configured WEB, created Controller

---
+++ src/main/java/com/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main(){
+        return new ModelAndView("index");
+    }
+}
